0b561fd2e053d10767ca85cb831e66fb409ce896
Surajkumar <surajkumar.kumar835@gmail.com>
2021-09-01 12:43:12 +0530

    Create boolean.java

---
+++ boolean.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Hello
+{
+    public static void main(String [] args)
+    {
+        Boolean b1=new Boolean(true);
+         Boolean b2=new Boolean(true);
+         boolean b3=true;
+         Boolean b4=true;
+         //== compare object reference not true in this case 
+         System.out.print(b1==b2);
+         //compare value 
+         System.out.print(b1==b3);
+            System.out.print(b3==b4);
+    }
+}
