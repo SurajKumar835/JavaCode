7943e11226d6e3805483d9365e0f6e8d2f5ae7a7
Surajkumar <surajkumar.kumar835@gmail.com>
2021-09-07 18:16:05 +0530

    Create wisCode.java

---
+++ wisCode.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Hello{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String s=in.nextLine();
+        String[] a=s.split(",");int i=0,j=0,c=0;
+        String[] str=new String[a.length];
+        for(i=0;i<a.length;i++)
+        {
+            for(j=i+1;j<a.length;j++)
+            {
+                if(Integer.parseInt(a[j])>Integer.parseInt(a[i]))
+                {
+                    str[c]=(a[i]+":"+a[j]);c++;
+                    a[i]="0";
+                    break;
+                }
+            }
+        }
+        for(i=0;i<a.length;i++)
+        {
+            if(Integer.parseInt(a[i])!=0)
+            {
+                str[c]=(a[i]+":"+"-1");c++;
+            }
+        }
+        int iMax=str.length-1;
+        System.out.print("[");
+            for(i=0;i<str.length;i++)
+            {
+                System.out.print("{"+str[i]+"}");
+                if(i!=iMax)System.out.print(",");
+            }
+        System.out.print("]");
+    }
+}
