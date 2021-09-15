d26447a72fd97eefd546f10107495d07c5906f6b
Surajkumar <surajkumar.kumar835@gmail.com>
2021-09-15 12:49:47 +0530

    Update General.java

--- General.java
+++ General.java
@@ -6,3 +6,23 @@ public class Hello
         System.out.print('b'+'i');
     }
 }
+///////////////////////
+//Java Example of instance Variable
+public class Hello
+{
+    static String hello="suraj";
+    int count=0;//instance variable not retain value
+        Hello(){
+            count++;
+            System.out.println(hello+":"+count);
+        }
+    public static void main(String[] args)
+    {
+       // int count=2;
+        Hello a=new Hello();
+        Hello b=new Hello();
+        Hello c=new Hello();
+        System.out.println(hello+"-"+c.count);
+    }
+}
+///////////////////////////////
