366a3b8d2eba59a585600cce62e74ba24b780531
Surajkumar <surajkumar.kumar835@gmail.com>
2021-09-20 15:06:35 +0530

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -12,5 +12,37 @@ public class MyClass {
       }
 
       }
+    public class MyClass {
+    public static void main(String args[]) {
+      
+      add(0,0);
+    }
+      static void add(int index,int start){
+          //System.out.println(index+":"+start+"*-----------");
+      for(int i=start;i<2;i++)
+      {
+         System.out.println(i+":"+start);
+            add(i,start+1);
+      }
+
+      }
+    
+}
+}
+/////////////////////////////////
+public class MyClass {
+    public static void main(String args[]) {
+      
+      add(0,0);
+    }
+      static void add(int index,int start){
+          //System.out.println(index+":"+start+"*-----------");
+      for(int i=start;i<2;i++)
+      {
+         System.out.println(i+":"+start);
+            add(i,start+1);
+      }
+
+      }
     
 }
