da3b25e76464966cdc3c2b9096f1d782d029b384
Surajkumar <surajkumar.kumar835@gmail.com>
2021-09-21 12:35:15 +0530

    Update SubArray.java

--- SubArray.java
+++ SubArray.java
@@ -27,3 +27,38 @@ public class TestClass {
          }
     }
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class TestClass {
+    public static void main(String args[] ) throws Exception {
+         // Write your code here
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         int N=Integer.parseInt(br.readLine());
+         String s=br.readLine().trim();
+         int Q=Integer.parseInt(br.readLine());
+         for(int i=0;i<Q;i++)
+         {
+             String lrt=br.readLine().trim();
+             String[] lr=lrt.split(" ");
+             int start=s.indexOf(lr[0]);
+             int end=s.indexOf(lr[1]);
+             int sum=0;
+            
+             String modS=s.substring(start,end+1); 
+             String[] str=modS.split("");
+             for(int j=0;j<str.length;j++)
+             {
+                 sum +=Integer.parseInt(str[j]);
+             }
+             if(sum%9==0)
+             {
+                 System.out.println("SAMEER");
+             }
+             else
+             {
+                 System.out.println("NO");
+             }
+         }
+    }
+}
